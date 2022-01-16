0d0dd12dfe73caddd5d6e931e065a8de330691c1
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-16 19:10:17 +0530

    javascript fixes

--- js/app.js
+++ js/app.js
@@ -2,35 +2,33 @@ window.addEventListener('mouseup', function (event) {
 	var searchbox = document.querySelector('.search-box');
 	var toggleMenu = document.querySelector('.menu');
 
+	/*search box*/
+	const searchToggle = document.querySelector('.toggle');
+	const search = document.querySelector('.search-box');
+
+	/*nav box*/
+	const navToggle = document.querySelector('#menu-toggle-btn');
+	const menu = document.querySelector('.menu');
+
 	if (event.target != searchbox && event.target.parentNode != searchbox) {
 		searchbox.classList.add('s-hide');
+		searchToggle.addEventListener('click', function () {
+			if (search.classList.contains('s-hide')) {
+				search.classList.remove('s-hide');
+			} else {
+				search.classList.add('s-hide');
+			}
+		});
 	}
 
 	if (event.target != toggleMenu && event.target.parentNode != toggleMenu) {
 		toggleMenu.classList.add('hide');
-	}
-});
-
-/*search box*/
-const searchToggle = document.querySelector('.toggle');
-const search = document.querySelector('.search-box');
-
-searchToggle.addEventListener('click', function () {
-	if (search.classList.contains('s-hide')) {
-		search.classList.remove('s-hide');
-	} else {
-		search.classList.add('s-hide');
-	}
-});
-
-/*nav box*/
-const navToggle = document.querySelector('#menu-toggle-btn');
-const menu = document.querySelector('.menu');
-
-navToggle.addEventListener('click', function () {
-	if (menu.classList.contains('hide')) {
-		menu.classList.remove('hide');
-	} else {
-		menu.classList.add('hide');
+		navToggle.addEventListener('click', function () {
+			if (menu.classList.contains('hide')) {
+				menu.classList.remove('hide');
+			} else {
+				menu.classList.add('hide');
+			}
+		});
 	}
 });
