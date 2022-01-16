e214f6c43d8d7dc184c2d47d7ad2654bc72188eb
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-16 11:30:33 +0530

    search box size increased, fix logo size 100*100px

--- functions.php
+++ functions.php
@@ -6,12 +6,10 @@ define( 'ltr_DEV_MODE', true );
 // Includes
 include( get_theme_file_path( '/inc/front/enqueue.php' ) );
 include( get_theme_file_path( '/inc/setup.php' ) );
-include( get_theme_file_path( '/inc/theme-customizer.php' ) );
 
 // Hooks
 add_action('wp_enqueue_scripts', 'ltr_enqueue'); //enqueue scripts
 add_action( 'after_setup_theme', 'ltr_setup_theme' ); 
 add_theme_support( 'post-thumbnails' ); // add support for featured image
-add_action( 'customize_register', 'ltr_customize_register' );
-add_action( 'wp_head', 'mytheme_customize_css' );
-add_theme_support( 'custom-logo' );
+// add_theme_support( 'custom-logo' ); 
+add_action( 'after_setup_theme', 'grigora_custom_logo_setup' );
--- inc/setup.php
+++ inc/setup.php
@@ -6,14 +6,13 @@ function ltr_setup_theme(){
 	
 }
 
-function themename_custom_logo_setup() {
+function grigora_custom_logo_setup() {
     $defaults = array(
         'height'               => 100,
-        'width'                => 400,
+        'width'                => 100,
         'flex-height'          => true,
         'flex-width'           => true,
         'header-text'          => array( 'site-title', 'site-description' ),
-        'unlink-homepage-logo' => true, 
     );
  
     add_theme_support( 'custom-logo', $defaults );
