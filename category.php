8b32fc4359a8d3411da56eeffc45c08c6c6524a9
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-17 13:11:50 +0530

    search icon changed in box

--- category.php
+++ category.php
@@ -1,3 +1,29 @@
 <?php get_header(); ?>
 
+<div class="container">
+    <section class="article">
+        <div class="archive-desc">
+            <h2 class="page-title"><?php single_cat_title(); ?></h2>
+            <p> <?php echo category_description(); ?></p>
+        </div>
+        <div class="post-container">
+
+            <?php
+
+            if ( have_posts() ) : 
+                while ( have_posts() ) : the_post(); 
+                   get_template_part('template-parts/posts/content-posts');
+                endwhile; 
+            endif;               
+
+            ?>
+
+        </div>
+
+        <?php the_posts_pagination(); ?>
+
+    </section>
+    <aside>Sidebar Area Will Design Later</aside>
+</div>
+
 <?php get_footer(); ?>
