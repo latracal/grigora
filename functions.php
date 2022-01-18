370cf8e03dd9fe45296f1c04906a7418d69baa70
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-18 16:49:25 +0530

    filename changed and function name changed

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ define( 'ltr_DEV_MODE', true );
 // Includes
 include( get_theme_file_path( '/inc/front/enqueue.php' ) );
 include( get_theme_file_path( '/inc/setup.php' ) );
-include( get_theme_file_path( '/inc/widgets.php' ) );
+include( get_theme_file_path( '/inc/sidebarfunction.php' ) );
 
 // Hooks
 add_action('wp_enqueue_scripts', 'ltr_enqueue'); //enqueue scripts
@@ -14,7 +14,7 @@ add_action( 'after_setup_theme', 'ltr_setup_theme' );
 add_theme_support( 'post-thumbnails' ); // add support for featured image
 add_action( 'after_setup_theme', 'grigora_custom_logo_setup' );
 add_filter( 'excerpt_more', 'change_excerpt_end_bracket' ); //Change excerpt end to ...
-add_action( 'widgets_init', 'grg_widgets' ); //sidebar
+add_action( 'widgets_init', 'grg_sidebar' ); //sidebar
 
 /**
  * Disable the emoji's
