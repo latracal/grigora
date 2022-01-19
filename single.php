ffea0bd715ad98074fb4c226fcf46720d57e6819
thirstycode <pratikkinage@gmail.com>
2022-01-19 11:28:17 +0530

    fixed cls

--- single.php
+++ single.php
@@ -6,7 +6,7 @@
         <h1 class="post-title"><?php the_title(); ?></h1>
         <span class="post-meta">Published by <?php echo nl2br(get_the_author_meta('display_name')); ?> on
             <?php echo get_the_date(); ?></span>
-        <img src="<?php the_post_thumbnail_url(); ?>" />
+        <?php the_post_thumbnail(); ?>
         <?php the_content(); ?>
     </section>
 
