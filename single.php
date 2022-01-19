34d0054a9a40c207b729c741fe88926e4db87734
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-19 10:29:40 +0530

    post template added

--- single.php
+++ single.php
@@ -1,9 +1,17 @@
-<?php
+<?php get_header(); ?>
 
-get_header( ); 
+<div class="container">
 
-?>
+    <section class="post-content">
+        <h1 class="post-title"><?php the_title(); ?></h1>
+        <span class="post-meta">Published by <?php echo nl2br(get_the_author_meta('display_name')); ?> on
+            <?php echo get_the_date(); ?></span>
+        <img src="<?php the_post_thumbnail_url(); ?>" />
+        <?php the_content(); ?>
+    </section>
 
+    <?php get_sidebar(); ?>
 
+</div>
 
 <?php get_footer( ); ?>
