d5069693a71fef8e95faac4da24e22cacee84272
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-20 17:34:50 +0530

    comment design

--- comments.php
+++ comments.php
@@ -5,28 +5,43 @@ if(post_password_required()){
 }
 ?>
 
-<div id="comments" class="comments-area">
-    <?php if ( ! comments_open() && get_comments_number() ) : ?>
-    <p class="no-comments"><?php _e( 'Comments are closed.' , 'twentythirteen' ); ?></p>
-    <?php endif; ?>
-    <?php if ( have_comments() ) :
-            ?>
-    <h2 class="comments-title">
-        Comments
-    </h2>
-    <ol class="comment-list">
-        <?php
-                    wp_list_comments( array(
-                        'style'       => 'ol',
-                        'short_ping'  => true,
-                        'avatar_size' => 74,
-                    ) );
-                ?>
-    </ol><!-- .comment-list -->
+<?php 
+
+    if( have_comments() ){
+        
+        ?>
+<h3><?php comments_number(); ?></h3>
+
+<ol class="comment-body">
+
     <?php
-         ?>
-    <?php endif; ?>
-    <?php comment_form(); ?>
-</div>
+
+    foreach( $comments as $comment )
+    {
+    ?>
+    <li class="comment">
+        <div class="author-det">
+            <div>
+                <?php echo get_avatar( $comment, 60 ); ?>
+            </div>
+            <div class="author-meta">
+                <span><?php comment_author(); ?></span>
+                <span><?php comment_date(); ?></span>
+            </div>
+        </div>
+        <div class="comment-text">
+            <?php comment_text(); ?>
+        </div>
+    </li>
+
+</ol>
+
 <?php
+
+}
+// comment_form();
+?>
+
+<?php
+}
 ?>
