09b48fbbaf137b99db3af958d1d3f209e7b8e36c
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-20 16:57:47 +0530

    author page mobile alignment changed

--- comments.php
+++ comments.php
@@ -4,28 +4,29 @@ if(post_password_required()){
     return;
 }
 ?>
-    <div id="comments" class="comments-area">
-        <?php if ( ! comments_open() && get_comments_number() ) : ?>
-            <p class="no-comments"><?php _e( 'Comments are closed.' , 'twentythirteen' ); ?></p>
-        <?php endif; ?>
-        <?php if ( have_comments() ) :
+
+<div id="comments" class="comments-area">
+    <?php if ( ! comments_open() && get_comments_number() ) : ?>
+    <p class="no-comments"><?php _e( 'Comments are closed.' , 'twentythirteen' ); ?></p>
+    <?php endif; ?>
+    <?php if ( have_comments() ) :
             ?>
-            <h2 class="comments-title">
-                Comments
-            </h2>
-            <ol class="comment-list">
-                <?php
+    <h2 class="comments-title">
+        Comments
+    </h2>
+    <ol class="comment-list">
+        <?php
                     wp_list_comments( array(
                         'style'       => 'ol',
                         'short_ping'  => true,
                         'avatar_size' => 74,
                     ) );
                 ?>
-            </ol><!-- .comment-list -->
-            <?php
+    </ol><!-- .comment-list -->
+    <?php
          ?>
-        <?php endif; ?>
-        <?php comment_form(); ?>
-    </div>
+    <?php endif; ?>
+    <?php comment_form(); ?>
+</div>
 <?php
 ?>
--- single.php
+++ single.php
@@ -71,7 +71,7 @@
         </div>
         <div class="related-posts">
             <?php
-                $related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 5, 'post__not_in' => array($post->ID) ) );
+                $related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 3, 'post__not_in' => array($post->ID) ) );
                 if( $related ) foreach( $related as $post ) {
                     setup_postdata($post); 
                     get_template_part('template-parts/posts/content-related');
