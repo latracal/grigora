62ce585c2029de7ee78634ae6c228df4ea0a8f4a
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-21 15:31:23 +0530

    heading removed from index page

--- index.php
+++ index.php
@@ -2,13 +2,7 @@
 
 <div class="container">
     <section class="article">
-        <div class="archive-desc">
-            <h2 class="page-title">Category Name</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et augue eu purus vehicula rhoncus.
-                Etiam at quam massa. Integer nulla ante, faucibus ut facilisis non, euismod eu est. Aenean vehicula
-                massa quis rutrum vestibulum. Vestibulum non viverra diam. Aenean ultrices interdum maximus. Integer sed
-                fermentum massa.</p>
-        </div>
+
         <div class="post-container">
 
             <?php
