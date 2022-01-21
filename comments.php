d9341c8f054f78138a261ea02c9f3cc9a11b36e6
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-21 13:26:56 +0530

    comment loop fixed

--- comments.php
+++ comments.php
@@ -37,29 +37,37 @@ if(post_password_required()){
         </div>
     </li>
 
+
+    <?php
+
+    }
+
+    ?>
 </ol>
 
 <?php
-
-}
 $comments_args = array(
-    // Change the title of send button 
-    'label_submit' => __( 'Post Comment', 'grg' ),
-    // Change the title of the reply section
-    'title_reply' => __( 'Your email address will not be published. Required fields are marked <span>*</span>', 'grg' ),
-    // Remove "Text or HTML to be displayed after the set of comment fields".
-    'comment_notes_after' => '',
-    // Redefine your own textarea (the comment body).
-    'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" rows="5" cols="50" name="comment" aria-required="true"></textarea></p>',
-    //Define Fields
-    'fields' => array(
-        //Author field
-        'author' => '<p class="comment-form-author"><label>Name</Name><br /><input id="author" name="author" aria-required="true" placeholder=""></input></p>',
-        //Email Field
-        'email' => '<p class="comment-form-email"><label>Email</label><br /><input id="email" name="email" placeholder=""></input></p>',
-        //URL Field
-        'url' => '<p class="comment-form-url"><label>Website</label><br /><input id="url" name="url" placeholder=""></input></p>',
-    ),
+// Change the title of send button
+'label_submit' => __( 'Post Comment', 'grg' ),
+// Change the title of the reply section
+'title_reply' => __( 'Your email address will not be published. Required fields are marked <span>*</span>', 'grg' ),
+// Remove "Text or HTML to be displayed after the set of comment fields".
+'comment_notes_after' => '',
+// Redefine your own textarea (the comment body).
+'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) .
+        '</label><br /><textarea id="comment" rows="5" cols="50" name="comment" aria-required="true"></textarea></p>',
+//Define Fields
+'fields' => array(
+//Author field
+'author' => '<p class="comment-form-author"><label>Name</Name><br /><input id="author" name="author"
+            aria-required="true" placeholder=""></input></p>',
+//Email Field
+'email' => '<p class="comment-form-email"><label>Email</label><br /><input id="email" name="email"
+        placeholder=""></input></p>',
+//URL Field
+'url' => '<p class="comment-form-url"><label>Website</label><br /><input id="url" name="url" placeholder=""></input></p>
+',
+),
 );
 
 comment_form($comments_args);
