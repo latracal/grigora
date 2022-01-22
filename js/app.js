7ddc16376a94ccd94a79fd5b30b40fbd614acc77
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-22 15:01:10 +0530

    submenu toggle event on mobile

--- js/app.js
+++ js/app.js
@@ -15,52 +15,53 @@ var menuicon = document.querySelector('.menu-toggle-icon');
 
 function togglesearch(event) {
 	menu.classList.add('hide');
-	if(searchinput.contains(event.target)){
+	if (searchinput.contains(event.target)) {
 		return;
 	}
 	if (searchbox.classList.contains('s-hide')) {
 		searchbox.classList.remove('s-hide');
 		searchinput.focus();
 		searchinput.select();
-		document.removeEventListener("click", clickoutsidesearchevent, false);
-		document.addEventListener("click", clickoutsidesearchevent, false);
+		document.removeEventListener('click', clickoutsidesearchevent, false);
+		document.addEventListener('click', clickoutsidesearchevent, false);
 	} else {
-		document.removeEventListener("click", clickoutsidesearchevent, false);
+		document.removeEventListener('click', clickoutsidesearchevent, false);
 		searchbox.classList.add('s-hide');
-	}
-  }
-
-function clickoutsidesearchevent(event){
-	if (event.target==searchbox||event.target==searchfield||event.target==searcharrowbutton || searchbtn.contains(event.target)){
-		;
-	}
-	else{
-		searchbox.classList.add('s-hide');
-		document.removeEventListener("click", clickoutsidesearchevent, false);
 	}
 }
 
-function clickoutsidemenuevent(event){
-	if (menu.contains(event.target) || menubtn.contains(event.target)){
-		;
+function clickoutsidesearchevent(event) {
+	if (
+		event.target == searchbox ||
+		event.target == searchfield ||
+		event.target == searcharrowbutton ||
+		searchbtn.contains(event.target)
+	) {
+	} else {
+		searchbox.classList.add('s-hide');
+		document.removeEventListener('click', clickoutsidesearchevent, false);
 	}
-	else{
+}
+
+function clickoutsidemenuevent(event) {
+	if (menu.contains(event.target) || menubtn.contains(event.target)) {
+	} else {
 		menu.classList.add('hide');
-		document.removeEventListener("click", clickoutsidemenuevent, false);
+		document.removeEventListener('click', clickoutsidemenuevent, false);
 	}
 }
 
-function togglemenu(event){
+function togglemenu(event) {
 	searchbox.classList.add('s-hide');
 	// if(menu.contains(event.target)){
 	// 	return;
 	// }
 	if (menu.classList.contains('hide')) {
 		menu.classList.remove('hide');
-		document.removeEventListener("click", clickoutsidemenuevent, false);
-		document.addEventListener("click", clickoutsidemenuevent, false);
+		document.removeEventListener('click', clickoutsidemenuevent, false);
+		document.addEventListener('click', clickoutsidemenuevent, false);
 	} else {
-		document.removeEventListener("click", clickoutsidemenuevent, false);
+		document.removeEventListener('click', clickoutsidemenuevent, false);
 		menu.classList.add('hide');
 	}
 }
@@ -68,5 +69,16 @@ function togglemenu(event){
 searchbtn.addEventListener('click', togglesearch, false);
 menubtn.addEventListener('click', togglemenu, false);
 
+//submenu toggle in mobile
+w = screen.width;
 
+const parent = document.querySelector('.menu-item-has-children');
+const child = document.querySelector('.sub-menu');
 
+parent.addEventListener('click', function () {
+	if (w < 768) {
+		child.style.display = 'block';
+	} else {
+		child.style.display = 'none';
+	}
+});
--- settings-functions/options-controls.php
+++ settings-functions/options-controls.php
@@ -1,8 +1,5 @@
 <?php
 
-
-
-
 if ( ! function_exists( 'girgora_options_menu' ) ) {
     function girgora_options_menu() {
         add_theme_page( 'Grigora Options', 'Grigora Options', 'manage_options', 'grigora-options', 'grigora_options_page' );
@@ -26,12 +23,18 @@ if ( ! function_exists( 'grigora_options_page' ) ) {
 
 if ( ! function_exists( 'grigora_options_page_elements' ) ) {
     function grigora_options_page_elements() {?>
-        hello
+        <div class="button-cover">
+            <div class="button-box r" id="button-8">
+                <input type="checkbox" class="checkbox-custom">
+                <div class="knobs">
+                    <span></span>
+                </div>
+                <div class="layer"></div>
+            </div>
+        </div>
     <?php }
 }
 
 add_action( 'admin_menu', 'girgora_options_menu' );
-
-include( get_theme_file_path( '/settings-functions/customizer-options/register-items.php' ) );
 
 ?>
