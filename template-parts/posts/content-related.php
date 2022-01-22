888b41bb20346ddcd2725f7de9c9f842dae9469b
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-01-22 12:39:00 +0530

    image placeholder added

--- template-parts/posts/content-related.php
+++ template-parts/posts/content-related.php
@@ -1,4 +1,13 @@
 <div>
+    <?php
+        if ( has_post_thumbnail($r->ID)) {
+    ?>
     <a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+    <?php
+    }else{
+    echo '<div class="img-placeholder"></div>';
+    }
+    ?>
+
     <h3><a href="<?php echo get_permalink(); ?>"><?php echo the_title(); ?></a></h3>
 </div>
