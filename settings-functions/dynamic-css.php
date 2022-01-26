ac7f86642ffd8b5a4d599d3e9f29c380e98196d2
thirstycode <pratikkinage@gmail.com>
2022-01-26 13:49:47 +0530

    colors flag

--- settings-functions/dynamic-css.php
+++ settings-functions/dynamic-css.php
@@ -3,10 +3,17 @@
 
 function grg_dynamic_customize_css() {
     $defaults = grigora_color_defaults();
+    $colors_flag = grigora_get_option("color");
     ?>
     <style type="text/css">
         body {
-        background-color: <?php echo get_theme_mod('grg_bg-color', $defaults['grg_bg-color']);
+        background-color: <?php
+            if($colors_flag){
+                echo get_theme_mod('grg_bg-color', $defaults['grg_bg-color']);
+            }
+            else{
+                echo $defaults['grg_bg-color'];
+            }
         ?>;
         }
     </style>
