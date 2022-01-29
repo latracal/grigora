c4bddd08fd963eaaff8b246046fe5071ac187fe6
thirstycode <pratikkinage@gmail.com>
2022-01-29 13:11:56 +0530

    fixed realtime font change in customizer

--- settings-functions/fonts-loading.php
+++ settings-functions/fonts-loading.php
@@ -3,26 +3,14 @@
 if (grigora_get_option("typography") && function_exists('google_fonts')){
 
     function grigora_add_google_fonts() {
-
-        $mods = get_theme_mods();
-
-        if(!$mods){
-            $mods = array();
+        $mods = array();
+        foreach(grigora_typography_defaults_fonts() as $key => $font){
+            array_push($mods, get_theme_mod($key, $font));
         }
 
-        $all_fonts = array();
-
-        foreach(grigora_typography_defaults_fonts() as $mod => $font) {
-            if(array_key_exists($mod, $mods)){
-                array_push($all_fonts, $mods[$mod]);
-            }
-            else{
-                array_push($all_fonts, $font);
-            }
-        }
-        $all_fonts = array_unique($all_fonts);
+        $mods = array_unique($mods);
         $font_request = 'https://fonts.googleapis.com/css?family=';
-        foreach ($all_fonts as $font) {
+        foreach ($mods as $key => $font) {
             $font_request = $font_request.google_fonts()[$font].'|';
         }
         wp_enqueue_style( 'grigora-google-fonts', $font_request, false ); 
