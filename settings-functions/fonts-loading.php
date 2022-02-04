3c9ac011ac2f1f59dfac4e0d30fdf3bf428913c8
thirstycode <pratikkinage@gmail.com>
2022-02-04 12:36:16 +0530

    fonts

--- settings-functions/fonts-loading.php
+++ settings-functions/fonts-loading.php
@@ -3,7 +3,23 @@
 if (grigora_get_option("typography") && function_exists('google_fonts')){
 
     function grigora_add_google_fonts() {
-        $safewebfonts = array("Arial", "Times New Roman", "Times", "Courier New", "Courier", "Verdana", "Georgia", "Palatino", "Garamond", "Bookman", "Tahoma", "Trebuchet MS", "Arial Black", "Impact", "Comic Sans MS");
+        $safewebfonts = array(
+            "Arial",
+            "Times New Roman",
+            "Times",
+            "Courier New",
+            "Courier",
+            "Verdana",
+            "Georgia",
+            "Palatino",
+            "Garamond",
+            "Bookman",
+            "Tahoma",
+            "Trebuchet MS",
+            "Arial Black",
+            "Impact",
+            "Comic Sans MS"
+        );
         $mods = array();
         foreach(grigora_typography_defaults_fonts() as $key => $font){
             array_push($mods, get_theme_mod($key, $font));
@@ -19,8 +35,9 @@ if (grigora_get_option("typography") && function_exists('google_fonts')){
         }
         if($count>0){
             $font_request = $font_request.'&display=fallback';
+            wp_enqueue_style( 'grigora-google-fonts', $font_request, false ); 
         }
-        wp_enqueue_style( 'grigora-google-fonts', $font_request, false ); 
+        
     }
          
     add_action( 'wp_enqueue_scripts', 'grigora_add_google_fonts' );
