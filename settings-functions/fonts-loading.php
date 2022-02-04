9d4a73d64a269bf01e1ffed79e12ed69d03dd2e3
thirstycode <pratikkinage@gmail.com>
2022-02-04 11:13:15 +0530

    system fonts ignore google fonts

--- settings-functions/fonts-loading.php
+++ settings-functions/fonts-loading.php
@@ -3,17 +3,23 @@
 if (grigora_get_option("typography") && function_exists('google_fonts')){
 
     function grigora_add_google_fonts() {
+        $safewebfonts = array("Arial", "Times New Roman", "Times", "Courier New", "Courier", "Verdana", "Georgia", "Palatino", "Garamond", "Bookman", "Tahoma", "Trebuchet MS", "Arial Black", "Impact", "Comic Sans MS");
         $mods = array();
         foreach(grigora_typography_defaults_fonts() as $key => $font){
             array_push($mods, get_theme_mod($key, $font));
         }
-
+        $count = 0;
         $mods = array_unique($mods);
         $font_request = 'https://fonts.googleapis.com/css?family=';
         foreach ($mods as $key => $font) {
-            $font_request = $font_request.google_fonts()[$font].'|';
+            if(!in_array($font, $safewebfonts)){
+                $font_request = $font_request.google_fonts()[$font].'|';
+                $count++;
+            }
         }
-        $font_request = $font_request.'&display=fallback';
+        if($count>0){
+            $font_request = $font_request.'&display=fallback';
+        }
         wp_enqueue_style( 'grigora-google-fonts', $font_request, false ); 
     }
          
