8986ae031ab572dc6a40a13cd0aad43a9638350e
thirstycode <pratikkinage@gmail.com>
2022-02-04 19:47:54 +0530

    docs

--- settings-functions/blog.php
+++ settings-functions/blog.php
@@ -5,7 +5,7 @@ if (grigora_get_option("blog")){
 	 * 
 	 * @since  1.000
 	 * 
-	 * @return number of words
+	 * @return int number of words
 	 */
     function grigora_custom_excerpt_length( $length ) {
         return get_theme_mod( 'grg_blog_archive_excerpt_words' , grigora_blog_defaults()['grg_blog_archive_excerpt_words']);
