3ac808bf1299a299258e023c5b3855788a3e8374
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-02-05 10:36:18 +0530

    searchbox outside menu container

--- header.php
+++ header.php
@@ -84,9 +84,9 @@
                             <path
                                 d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                         </svg></button>
-                    <?php get_search_form(); ?>
                 </div>
             </div>
+            <?php get_search_form(); ?>
         </nav>
         <nav class="mobile-header">
             <div class="site-branding" itemtype="<?php echo grg_get_schema_tag('organization')['itemtype'] ?>"
