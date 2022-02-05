c6e619093673d8a90d9c43821d4ab624f23ed151
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-02-05 12:47:23 +0530

    input field with css

--- compatibility/init.php
+++ compatibility/init.php
@@ -2,3 +2,4 @@
 
 
 include( get_theme_file_path( '/compatibility/woocommerce/woocommerce-compat.php' ) );
+wp_enqueue_style( 'woocommerce-css', get_template_directory_uri() . '/dist/css/woocommerce.css' );
