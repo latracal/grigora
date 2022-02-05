675e3b2b223c0d8bb0a07b8dbc68732934dea182
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-02-05 15:42:19 +0530

    added hook to put woocommerce in div

--- compatibility/woocommerce/woocommerce-compat.php
+++ compatibility/woocommerce/woocommerce-compat.php
@@ -1,4 +1,9 @@
 <?php
 
 
-// todo all woo commerce things here
+// custom div using hooks
+
+add_action( 'woocommerce_before_main_content', 'wcm_container_open', 5 );
+add_action( 'dynamic_sidebar_after', 'wcm_container_close', 5 );
+add_action('woocommerce_before_shop_loop', 'wcm_product_open', 10);
+add_action('loop_start', 'wcm_product_close', 1); 
