8449d39b8d5f13e8ef0f075d3f7beb2f0b6989af
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-02-23 13:05:51 +0530

    content renders

--- archive.php
+++ archive.php
@@ -5,21 +5,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-
 <?php get_header(); ?>
 
-<?php
+<div class="container">
+    <section class="article">
 
+        <div class="post-container">
 
-    if ( $the_query->have_posts() ) {
-        
-        while ( $the_query->have_posts() ) {
-            $the_query->the_post();
-            get_template_part('template-parts/posts/content');
-        }
-        
-    }
+            <?php
 
-?>
+            if ( have_posts() ) : 
+                while ( have_posts() ) : the_post(); 
+                   get_template_part('template-parts/posts/content-posts');
+                endwhile; 
+            endif;               
+
+            ?>
+
+        </div>
+
+        <?php the_posts_pagination(); ?>
+
+    </section>
+
+    <?php get_sidebar(); ?>
+
+</div>
 
 <?php get_footer(); ?>
