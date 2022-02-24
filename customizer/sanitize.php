24fd20e604ae82f9a904b31e2b3a1a6e5680aafc
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-02-24 13:48:13 +0530

    Function exists corrected

--- customizer/sanitize.php
+++ customizer/sanitize.php
@@ -4,9 +4,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // For security
 }
 
-if(!functionexists('grg_sanitize_select')){
+if(!function_exists('grg_sanitize_select')){
 	//select sanitization function
 	function grg_sanitize_select( $input, $setting ){
+			
+		$input = sanitize_key($input);
+
+		$choices = $setting->manager->get_control( $setting->id )->choices;
+						
+		return ( array_key_exists( $input, $choices ) ? $input : $setting->default );                
+		
+	}
+}
+
+if(!function_exists('grg_sanitize_range')){
+	//select sanitization function
+	function grg_sanitize_range( $input, $setting ){
 			
 		$input = sanitize_key($input);
 
