bab522833dbfd7e2fca178b8cf60790519d18f96
Trushar10 <63665542+Trushar10@users.noreply.github.com>
2022-02-27 12:28:26 +0530

    rtl css added for index page

--- inc/front/enqueue.php
+++ inc/front/enqueue.php
@@ -16,6 +16,8 @@ function grg_global_enqueue(){
 	
 	//endueue style sheet to header
 	wp_enqueue_style('grg_global_style', $uri . '/dist/css/global.min.css', [], $ver);	
+	wp_enqueue_style('grg_global_style_rtl', $uri . '/dist/css/style-rtl.min.css', [], $ver);	
+	wp_style_add_data( 'grg_global_style', 'grg_global_style_rtl', 'replace' );
     
     //use false as laster parameter to load script in footer
 	wp_register_script('grg_global_scripts1', $uri . '/js/app.js', [], $ver, true);
